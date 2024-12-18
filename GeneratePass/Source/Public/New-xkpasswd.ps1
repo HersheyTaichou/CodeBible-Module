5b8f4db3ef08290fe7339292fbde87a2477f6dee
Mike Hiersche <hierschemichael@gmail.com>
2024-12-18 10:19:37 -0500

    Updates GeneratePass to use Plaster with gyPSum

--- GeneratePass/Source/Public/New-xkpasswd.ps1
+++ GeneratePass/Source/Public/New-xkpasswd.ps1
@@ -1,10 +1,3 @@
-#XKCD PASSWORD GENERATOR
-
-#Load example dictionary file at import
-#. $PSScriptRoot\dictionary.ps1
-
-#Write-Host $PSScriptRoot
-
 <#
 .SYNOPSIS
 This function creates random passwords using user-defined characteristics. It is inspired by the XKCD 936 comic and the password generator spawned from it, xkpasswd.net.
@@ -686,67 +679,5 @@ function New-xkpasswd {
         } else {
             return $PasswordArray
         }
-    }
-}
-
-<#
-.SYNOPSIS
-This function will return a random number.
-
-.DESCRIPTION
-This function takes two numbers and returns a random number between those two numbers. It defaults to the minimum and maximum possible value for an integer, which is a range of 4,294,967,295
-
-.PARAMETER Min
-The minimum number to consider. Defaults to the minimum value for an integer (-2147483648)
-
-.PARAMETER Max
-The maximum number to consider. Defaults to the maximum value for an integer (2147483647)
-
-.EXAMPLE
-Get-RandomInt -Minimum 0 -Maximum 100
-
-81
-
-This example generates a random number between 0 and 100
-
-.EXAMPLE
-Get-RandomInt -Minimum 0
-
-1461304439
-
-This example generates a random number between 0 and [Int]::MaxValue
-
-.EXAMPLE
-Get-RandomInt
-
--1728936647
-
-This example generates a random number between [Int]::minValue and [Int]::MaxValue
-
-.NOTES
-General notes
-#>
-function Get-RandomInt {
-    [CmdletBinding()]
-    param (
-        # The lowest number to include. Defaults to the lowest possible number
-        [Parameter()][int]$Minimum = [Int]::MinValue,
-        
-        # the highest number to include
-        [Parameter()][int]$Maximum = [Int]::MaxValue
-    )
-    
-    process {
-        # Check to make sure minimum is less then maximum, then get a random number
-        if ($Minimum -lt $Maximum) {
-            $Return = [System.Security.Cryptography.RandomNumberGenerator]::GetInt32($Minimum,$Maximum)
-        } else {
-            Write-Warning 'Minimum length must be less than Maximum length'
-            return -1
-        }
-    }
-    
-    end {
-        return $Return
     }
 }
